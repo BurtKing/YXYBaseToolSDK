b05a084f4e6640e99796d92e56a919462a7d6a51
王保添 <wbt1022@sina.com>
2022-01-05 14:10:03 +0800

    no message

--- YXYBaseToolSDK/Classes/BBUtils.m
+++ YXYBaseToolSDK/Classes/BBUtils.m
@@ -11,8 +11,7 @@
 
 @implementation BBUtils
 //MD5加密方法
-+(NSString *)md5:(NSString *)inPutText
-{
++(NSString *)md5:(NSString *)inPutText{
     const char *cStr = [inPutText UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
     CC_MD5(cStr, (int)strlen(cStr), result);
@@ -24,22 +23,23 @@
              result[12], result[13], result[14], result[15]
              ]lowercaseString];
 }
-//比较两个数字字符串之间的大小
-+(BOOL)compare:(NSString*) A oldString: (NSString*) B
-{
-    if ([A intValue]>[B intValue] || [A intValue]==[B intValue])
-    {
-        return YES;
+
++(NSString *)sha1:(NSString *)input{
+    const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [NSData dataWithBytes:cstr length:input.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    CC_SHA1(data.bytes, (unsigned int)data.length, digest);
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i=0; i<CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
     }
-    else
-    {
-        return NO;
-    }
+    return output;
 }
 
 //获取当前时间
-+(NSString*)getCurrentTime
-{
++(NSString*)getCurrentTime{
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     [formatter setDateFormat:@"YYYY-MM-dd HH:mm:ss"];
     NSDate *datenow = [NSDate date];
@@ -47,7 +47,53 @@
     return currentTimeString;
 }
 
++(NSString *)getCurrentYMD{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YYYY-MM-dd"];
+    NSDate *datenow = [NSDate date];
+    NSString *currentTimeString = [formatter stringFromDate:datenow];
+    return currentTimeString;
+}
 
++(NSString *)getCurrentHM{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"HH:mm"];
+    NSDate *datenow = [NSDate date];
+    NSString *currentTimeString = [formatter stringFromDate:datenow];
+    return currentTimeString;
+}
+
+//比较两个数字字符串之间的大小
++(BOOL)compare:(NSString*) A oldString: (NSString*) B{
+    if ([A intValue]>[B intValue] || [A intValue]==[B intValue]) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
++(NSString *)getNowTimeTimestamp{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init] ;
+    
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    [formatter setDateFormat:@"YYYY-MM-dd HH:mm:ss"]; // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    
+    //设置时区,这个对于时间的处理有时很重要
+    
+    NSTimeZone* timeZone = [NSTimeZone timeZoneWithName:@"Asia/Shanghai"];
+    
+    [formatter setTimeZone:timeZone];
+    
+    NSDate *datenow = [NSDate date];//现在时间,你可以输出来看下是什么格式
+    
+    NSString *timeSp = [NSString stringWithFormat:@"%ld", (long)[datenow timeIntervalSince1970]];
+    
+    return timeSp;
+}
 
 +(BOOL)isMobel:(NSString*)phoneNum
 {
@@ -114,6 +160,14 @@
 }
 
 
++(BOOL)isPassWord:(NSString *)str{
+//    NSString * regex = @"^(?![0-9]+$)(?![a-zA-Z]+$)(?![-~!@#\\\\$%^&*_+=|(){}\\[:;\"'<>,.?\\]\\`]+$)[0-9a-zA-Z-~!@#\\\\$%^&*_+=|(){}\\[:;\"'<>,.?\\]\\`]{8,16}$";
+    NSString *regex = @"^(?![a-z]+$)(?![A-Z]+$)(?![0-9]+$)(?![\\W_]+$)(?![a-zA-Z]+$)(?![a-z0-9]+$)(?![a-z\\W_]+$)(?![A-Z0-9]+$)(?![A-Z\\W_]+$)(?![0-9\\W_]+$)[a-zA-Z0-9\\W_]{8,16}$";
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    BOOL isMatch = [pred evaluateWithObject:str];
+    return isMatch;
+}
+
 +(UIViewController *)getCurrentVC {
     //获得当前活动窗口的根视图
     UIViewController* vc = [UIApplication sharedApplication].keyWindow.rootViewController;
@@ -156,6 +210,58 @@
     
 }
 
++(id)getFirstResponder{
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    UIView *firstResponder = [keyWindow performSelector:@selector(firstResponder)];
+    return firstResponder;
+}
+
+/**
+ ** lineView:      需要绘制成虚线的view
+ ** lineLength:    虚线的宽度 //2
+ ** lineSpacing:        虚线的间距//1
+ ** lineColor:    虚线的颜色
+ **/
++ (void)drawDashLine:(UIView *)lineView lineLength:(int)lineLength lineSpacing:(int)lineSpacing lineColor:(UIColor *)lineColor{
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    [shapeLayer setBounds:lineView.bounds];
+    [shapeLayer setPosition:CGPointMake(CGRectGetWidth(lineView.frame) / 2, CGRectGetHeight(lineView.frame))];
+    [shapeLayer setFillColor:[UIColor clearColor].CGColor];
+    //  设置虚线颜色为blackColor
+    [shapeLayer setStrokeColor:lineColor.CGColor];
+    //  设置虚线宽度
+    [shapeLayer setLineWidth:CGRectGetHeight(lineView.frame)];
+    [shapeLayer setLineJoin:kCALineJoinRound];
+    //  设置线宽，线间距
+    [shapeLayer setLineDashPattern:[NSArray arrayWithObjects:[NSNumber numberWithInt:lineLength], [NSNumber numberWithInt:lineSpacing], nil ]];
+    //  设置路径
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, 0, 0);
+    CGPathAddLineToPoint(path, NULL, CGRectGetWidth(lineView.frame), 0);
+    [shapeLayer setPath:path];
+    CGPathRelease(path);
+    //  把绘制好的虚线添加上来
+    [lineView.layer addSublayer:shapeLayer];
+    
+}
+
++(void)touchFeedBack{
+    if (@available(iOS 11.0, *)) {
+        UIImpactFeedbackGenerator *feedBackGenertor = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
+        [feedBackGenertor impactOccurred];
+    }
+}
+
+
++(NSString *)removeWhiteSpaceAndNewlineChar:(NSString *)string{
+    NSMutableString *mutStr = [NSMutableString stringWithString:string];
+    NSRange range = {0,string.length};
+    [mutStr replaceOccurrencesOfString:@" " withString:@"" options:NSLiteralSearch range:range];
+    NSRange range2 = {0,mutStr.length};
+    [mutStr replaceOccurrencesOfString:@"\n"withString:@""options:NSLiteralSearch range:range2];
+    return mutStr;
+}
 
 /// 添加四边阴影效果
 + (void)addShadowToView:(UIView *)theView withColor:(UIColor *)theColor {
@@ -269,12 +375,12 @@
     }
 }
 
-// 密码校验
-+ (BOOL)isPassWord:(NSString *)str{
-    NSString *retxtpwd = @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{8}$";
-    NSPredicate *pwdTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",retxtpwd];
-    return [pwdTest evaluateWithObject:str];
-}
+//// 密码校验
+//+ (BOOL)isPassWord:(NSString *)str{
+//    NSString *retxtpwd = @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{8}$";
+//    NSPredicate *pwdTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",retxtpwd];
+//    return [pwdTest evaluateWithObject:str];
+//}
 
 // 账号校验
 + (BOOL)isAcount:(NSString *)str{
@@ -359,6 +465,218 @@
     CGImageRelease(bitmapImage);
 
     return [UIImage imageWithCGImage:scaledImage]; // 黑白图片
+}
+
++(BOOL)isPureFloat:(NSString *)string{
+
+    NSScanner* scan = [NSScanner scannerWithString:string];
+
+    float val;
+
+    return [scan scanFloat:&val] && [scan isAtEnd];
+
+}
+
+ 
+
++(BOOL)isPureInt:(NSString *)string{
+
+    NSScanner* scan = [NSScanner scannerWithString:string];
+
+    int val;
+
+    return [scan scanInt:&val] && [scan isAtEnd];
+
+}
+
++(UIImage *)imageFromBase64String:(NSString *)str{
+    NSString *imgStr = str;
+    if ([str hasPrefix:@"data:image/png;base64,"]) {
+        NSRange range = [imgStr rangeOfString:@"data:image/png;base64,"];
+        imgStr = [imgStr substringFromIndex:range.length];
+    }
+    NSData *imageData = [[NSData alloc] initWithBase64EncodedString:imgStr options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    return [UIImage imageWithData:imageData];
+}
+
+//处理特殊字符
++ (NSString *)turnURLChineseChar:(NSString *)str {
+   NSString *newString = str;
+   //遍历字符串中的字符
+   for(int i=0; i< [str length];i++){
+       int a = [str characterAtIndex:i];
+       //汉字的处理
+       if( a > 0x4e00 && a < 0x9fff)
+       {
+           NSString *oldString = [str substringWithRange:NSMakeRange(i, 1)];
+           NSString *string = [oldString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+           newString = [newString stringByReplacingOccurrencesOfString:oldString withString:string];
+       }
+       //空格处理
+       if ([newString containsString:@" "]) {
+           newString = [newString stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+
+       }
+     //如果需要处理其它特殊字符,在这里继续判断处理即可.
+   }
+   return newString;
+}
+
+//判断是否大写
++(BOOL)onlyInputACapital:(NSString*)string {
+    NSString *regex =@"[A-Z]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    BOOL inputString = [predicate evaluateWithObject:string];
+    return inputString;
+}
+//验证车牌号是否正确
++(BOOL)checkCarID:(NSString *)carID {
+    NSString *carRegex = @"^[测京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{5}[A-Z0-9挂警学台港澳]{1}$";
+    NSPredicate *carTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",carRegex];
+    BOOL isFit = [carTest evaluateWithObject:carID];
+
+    NSString *carRegex1 = @"^[测京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂警学台港澳]{1}$";
+    NSPredicate *carTest1 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",carRegex1];
+    BOOL isFit1 = [carTest1 evaluateWithObject:carID];
+
+
+    NSString *carRegex2 = @"^[货]{1}[A-Z0-9]{1,10}$";
+    NSPredicate *carTest2 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",carRegex2];
+    BOOL isFit2 = [carTest2 evaluateWithObject:carID];
+
+
+    return isFit || isFit1 || isFit2;
+    
+}
+//验证纳税人识别号
++ (BOOL)validateTaxpayerNumber:(NSString*)TaxpayerNumber {
+    if (TaxpayerNumber.length==15 || TaxpayerNumber.length==18 ||TaxpayerNumber.length==20) {
+        NSString *regex = @"[A-Z0-9]*|[A-Z]*|[0-9]*";
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+        return [pred evaluateWithObject:TaxpayerNumber];
+    } else {
+        return NO;
+    }
+}
+
++(BOOL)validateIDCardNumber:(NSString *)value {
+    
+    value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSInteger length =0;
+    if (!value) {
+        return NO;
+    }else {
+        length = value.length;
+        //不满足15位和18位，即身份证错误
+        if (length !=15 && length !=18) {
+            return NO;
+        }
+    }
+    // 省份代码
+    NSArray *areasArray = @[@"11",@"12", @"13",@"14", @"15",@"21", @"22",@"23", @"31",@"32", @"33",@"34", @"35",@"36", @"37",@"41", @"42",@"43", @"44",@"45", @"46",@"50", @"51",@"52", @"53",@"54", @"61",@"62", @"63",@"64", @"65",@"71", @"81",@"82", @"91"];
+    
+    // 检测省份身份行政区代码
+    NSString *valueStart2 = [value substringToIndex:2];
+    BOOL areaFlag =NO; //标识省份代码是否正确
+    for (NSString *areaCode in areasArray) {
+        if ([areaCode isEqualToString:valueStart2]) {
+            areaFlag =YES;
+            break;
+        }
+    }
+    
+    if (!areaFlag) {
+        return NO;
+    }
+    
+    NSRegularExpression *regularExpression;
+    NSUInteger numberofMatch;
+    
+    int year =0;
+    //分为15位、18位身份证进行校验
+    switch (length) {
+        case 15:
+            //获取年份对应的数字
+            year = [value substringWithRange:NSMakeRange(6,2)].intValue +1900;
+            
+            if (year %4 ==0 || (year %100 ==0 && year %4 ==0)) {
+                //创建正则表达式 NSRegularExpressionCaseInsensitive：不区分字母大小写的模式
+                regularExpression = [[NSRegularExpression alloc]initWithPattern:@"^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}$"
+                                                                        options:NSRegularExpressionCaseInsensitive error:nil];//测试出生日期的合法性
+            }else {
+                regularExpression = [[NSRegularExpression alloc]initWithPattern:@"^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}$"
+                                                                        options:NSRegularExpressionCaseInsensitive error:nil];//测试出生日期的合法性
+            }
+            //使用正则表达式匹配字符串 NSMatchingReportProgress:找到最长的匹配字符串后调用block回调
+            numberofMatch = [regularExpression numberOfMatchesInString:value
+                                                               options:NSMatchingReportProgress
+                                                                 range:NSMakeRange(0, value.length)];
+            
+            if(numberofMatch >0) {
+                return YES;
+            }else {
+                return NO;
+            }
+        case 18:
+            year = [value substringWithRange:NSMakeRange(6,4)].intValue;
+            if (year %4 ==0 || (year %100 ==0 && year %4 ==0)) {
+                regularExpression = [[NSRegularExpression alloc]initWithPattern:@"^((1[1-5])|(2[1-3])|(3[1-7])|(4[1-6])|(5[0-4])|(6[1-5])|71|(8[12])|91)\\d{4}(((19|20)\\d{2}(0[13-9]|1[012])(0[1-9]|[12]\\d|30))|((19|20)\\d{2}(0[13578]|1[02])31)|((19|20)\\d{2}02(0[1-9]|1\\d|2[0-8]))|((19|20)([13579][26]|[2468][048]|0[048])0229))\\d{3}(\\d|X|x)?$" options:NSRegularExpressionCaseInsensitive error:nil];//测试出生日期的合法性
+            }else {
+                regularExpression = [[NSRegularExpression alloc]initWithPattern:@"^((1[1-5])|(2[1-3])|(3[1-7])|(4[1-6])|(5[0-4])|(6[1-5])|71|(8[12])|91)\\d{4}(((19|20)\\d{2}(0[13-9]|1[012])(0[1-9]|[12]\\d|30))|((19|20)\\d{2}(0[13578]|1[02])31)|((19|20)\\d{2}02(0[1-9]|1\\d|2[0-8]))|((19|20)([13579][26]|[2468][048]|0[048])0229))\\d{3}(\\d|X|x)?$" options:NSRegularExpressionCaseInsensitive error:nil];//测试出生日期的合法性
+            }
+            numberofMatch = [regularExpression numberOfMatchesInString:value
+                                                               options:NSMatchingReportProgress
+                                                                 range:NSMakeRange(0, value.length)];
+            
+            
+            if(numberofMatch >0) {
+                //1：校验码的计算方法 身份证号码17位数分别乘以不同的系数。从第一位到第十七位的系数分别为：7－9－10－5－8－4－2－1－6－3－7－9－10－5－8－4－2。将这17位数字和系数相乘的结果相加。
+                
+                int S = [value substringWithRange:NSMakeRange(0,1)].intValue*7 + [value substringWithRange:NSMakeRange(10,1)].intValue *7 + [value substringWithRange:NSMakeRange(1,1)].intValue*9 + [value substringWithRange:NSMakeRange(11,1)].intValue *9 + [value substringWithRange:NSMakeRange(2,1)].intValue*10 + [value substringWithRange:NSMakeRange(12,1)].intValue *10 + [value substringWithRange:NSMakeRange(3,1)].intValue*5 + [value substringWithRange:NSMakeRange(13,1)].intValue *5 + [value substringWithRange:NSMakeRange(4,1)].intValue*8 + [value substringWithRange:NSMakeRange(14,1)].intValue *8 + [value substringWithRange:NSMakeRange(5,1)].intValue*4 + [value substringWithRange:NSMakeRange(15,1)].intValue *4 + [value substringWithRange:NSMakeRange(6,1)].intValue*2 + [value substringWithRange:NSMakeRange(16,1)].intValue *2 + [value substringWithRange:NSMakeRange(7,1)].intValue *1 + [value substringWithRange:NSMakeRange(8,1)].intValue *6 + [value substringWithRange:NSMakeRange(9,1)].intValue *3;
+                
+                //2：用加出来和除以11，看余数是多少？余数只可能有0－1－2－3－4－5－6－7－8－9－10这11个数字
+                int Y = S %11;
+                NSString *M =@"F";
+                NSString *JYM =@"10X98765432";
+                M = [JYM substringWithRange:NSMakeRange(Y,1)];// 3：获取校验位
+                
+                NSString *lastStr = [value substringWithRange:NSMakeRange(17,1)];
+                
+                NSLog(@"%@",M);
+                NSLog(@"%@",[value substringWithRange:NSMakeRange(17,1)]);
+                //4：检测ID的校验位
+                if ([lastStr isEqualToString:@"x"]) {
+                    if ([M isEqualToString:@"X"]) {
+                        return YES;
+                    }else{
+                        return NO;
+                    }
+                }else{
+                    if ([M isEqualToString:[value substringWithRange:NSMakeRange(17,1)]]) {
+                        return YES;
+                    }else {
+                        return NO;
+                    }
+                }
+            }else {
+                return NO;
+            }
+        default:
+            return NO;
+    }
+}
+
++(BOOL)judgeVIN:(NSString *)str
+{
+    if (!str || str.length != 17) {
+        return NO;
+    }
+    if ([str containsString:@"I"]||[str containsString:@"O"]||[str containsString:@"Q"])
+    {
+        return NO;
+    }
+    
+    return YES;
 }
 
 @end
