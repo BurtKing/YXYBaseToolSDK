ada75738a23aad56049c2448a40c9d49056f415b
王保添 <wbt1022@sina.com>
2022-01-07 11:47:33 +0800

    Theme Header

---
+++ YXYBaseToolSDK/Classes/ThemeHeader.h
@@ -0,0 +1,47 @@
+//
+//  ThemeHeader.h
+//  Insurance
+//
+//  Created by Burt on 2020/12/10.
+//  Copyright © 2020 com.uqiauto. All rights reserved.
+//
+
+#ifndef ThemeHeader_h
+#define ThemeHeader_h
+
+
+//主基调
+#define KThemeColor [UIColor colorWithHexString:@"#0F6EFF"]//主色
+#define KSubColor [UIColor colorWithHexString:@"#D4D9E1"]//辅助色
+#define KBackgroundColor [UIColor colorWithHexString:@"#F3F6FA"]//背景色
+
+//文字色
+#define KTitleColor [UIColor colorWithHexString:@"#282D3B"]//标题 强调信息
+#define KContentColor [UIColor colorWithHexString:@"#8491A3"]//正文
+#define KSubInfoColor [UIColor colorWithHexString:@"#555F71"]//辅助信息 弱按钮文字 常规按钮描边
+#define KDisabbleAndPlaceholderColor [UIColor colorWithHexString:@"#B8BFC9"]//不可点击 输入框提示
+#define KPartBackgroundColor [UIColor colorWithHexString:@"#F0F0F0"]//部分背景色 边框色
+#define KCardBackgroundColor [UIColor colorWithHexString:@"#FFFFFF"]//卡片背景 深色背景文字
+
+//其他辅色
+#define KEmergencyColor [UIColor colorWithHexString:@"#FE5252"]//紧急
+#define KImportantColor [UIColor colorWithHexString:@"#FF8456"]//重要
+#define KMinorColor [UIColor colorWithHexString:@"#FFB608"]//次要
+#define KNormalColor [UIColor colorWithHexString:@"#51D2A8"]//正常
+#define KTipsColor [UIColor colorWithHexString:@"#489FFF"]//提示
+#define KInvalidColor [UIColor colorWithHexString:@"#86A1D3"]//失效
+
+
+
+//字体
+#define KSpecialFont [UIFont systemFontOfSize:30]//特殊字体
+#define KTitleFont [UIFont systemFontOfSize:24]//标题 titlebar
+#define KListTitleFont [UIFont systemFontOfSize:22]//列表标题 大按钮文字
+#define KContentFont [UIFont systemFontOfSize:18]//次要信息 内容 多数文字
+#define KSubInfoFont [UIFont systemFontOfSize:15]//辅助信息 摘要 标签文字 引导 弱提示
+#define KBageFont [UIFont systemFontOfSize:12]//角标
+
+
+#define EFCOLOR [UIColor colorWithHexString:@"#efeff4"]
+
+#endif /* ThemeHeader_h */
